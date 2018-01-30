b842b1fe0437f8f50ba482218d4b7ba342e7a2e5
HirakiHirotaka <e155711@ie.u-ryukyu.ac.jp>
2018-01-30 18:45:26 +0900

    edit main.sh
    ユーザーがselectしたものを表示できるようにした.

--- convert_Shell/main.sh
+++ convert_Shell/main.sh
@@ -10,9 +10,13 @@ MSG1 = `./convert_neo.neo`
 
 
 echo "何から何に拡張子を変更したいか選んでください"
-select VAR in PNGtoPDF
+select VAR in "PNGtoPDF"
 do
+  echo "あなたが選んだのは $VAR です"
+  # if [$VAR = "PNGtoPDF"]; then
+  #   echo $MSG1
+  # fi
   case $VAR in
-    "PNGtoPDF") echo $MSG1;;
+    "PNGtoPDF") echo $MSG1 ;;
   esac
 done
