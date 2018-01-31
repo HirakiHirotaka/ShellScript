78c3adb7470a757b0d7d9f944697cb9a1fd7d3b0
HirakiHirotaka <e155711@ie.u-ryukyu.ac.jp>
2018-01-31 12:45:59 +0900

    edit other Shellfile

--- convert_Shell/PDFtoJPEG.sh
+++ convert_Shell/PDFtoJPEG.sh
@@ -3,18 +3,22 @@
 # 保存名を「名前+数字」(例 千本桜1.png)で保存する
 # 実行にはsh ./convert_neo.shで行うことができる
 
-echo "保存名の名前の部分を記入してください"
-echo "例) 千本桜1.pdfの場合 「千本桜」"
+echo "========================================"
+echo ">保存名の名前の部分を記入してください"
+echo ">例) 千本桜1.pdfの場合 「千本桜」"
 read name
-echo "保存名は $name です"
-echo "convertする回数を入力してください"
+echo ">保存名は $name です"
+echo ">convertする回数を入力してください"
 read kaisuu
-echo "$kaisuu 回convertします"
+echo ">$kaisuu 回convertします"
 
-echo "実行中,,,,,"
+cd images
+
+echo ">実行中,,,,,"
 for CNT in `seq 1 +1 $kaisuu`
 do
   convert $name$CNT.pdf $name$CNT.jpg
-
 done
-echo "終了!動作は正常に終了しました."
+echo ">終了!動作は正常に終了しました."
+echo "========================================"
+cd ../
