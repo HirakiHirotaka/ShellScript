bb19ffbc805e332cd9d2e66c68b1304c64e0c5d6
HirakiHirotaka <e155711@ie.u-ryukyu.ac.jp>
2018-01-31 12:56:07 +0900

    edit main.shell メニューから実行

--- convert_Shell/main.sh
+++ convert_Shell/main.sh
@@ -11,6 +11,11 @@ select VAR in "JPEGtoPDF" "JPEGtoPNG" "PNGtoPDF" "PNGtoJPEG" "PDFtoJPEG" "PDFtoP
 do
   echo "あなたが選んだのは $VAR です.実行画面に移ります."
   case $VAR in
+    "JPEGtoPDF") sh ./JPEGtoPDF.sh ;;
+    "JPEGtoPNG") SH ./JPEGtoPNG.sh ;;
     "PNGtoPDF") sh ./PNGtoPDF.sh ;;
+    "PNGtoJPEG") sh ./PNGtoJPEG.sh ;;
+    "PDFtoPNG") sh ./PDFtoPNG.sh ;;
+    "PDFtoJPEG") sh ./PDFtoJPEG.sh ;;
   esac
 done
